09e5105b3a98b693c0b75251faece94076cb53cd
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-10-25 15:35:23 +0800

    create signup component

---
+++ src/app/components/pages/signup/signup.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [RouterModule, ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  get fc() {
+    return this.signupForm.controls;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log('Form Submitted!', this.signupForm.value);
+      // Here you can make API call or other actions with form data
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
